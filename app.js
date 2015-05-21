04fd5eada5a489a517485e95b7b6454d9254011b
ip821 <ip821@yandex.ru>
2015-05-21 23:03:43 +0300

    Fixes.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-﻿var gui = require('nw.gui');
+﻿ $ = require("jquery");
+ global.jQuery = $;
+global.document = window.document;
+var gui = require('nw.gui');
+var bootstrap = require("bootstrap");
+
 gui.Window.get().show();
 gui.Window.get().showDevTools();
 
-var $ = require("jquery");
 var selector = $("#left");
+selector.prop("value", 123);
