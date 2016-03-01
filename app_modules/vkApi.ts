f57f84bd105c5c90ea71e85cb7dc8fec472e9671
igor <igor>
2016-03-01 19:42:06 +0200

    Refactoring.

--- app_modules/vkApi.ts
+++ app_modules/vkApi.ts
@@ -1,9 +1,10 @@
+import $ = require('jquery');
+import _ = require('underscore');
+
 var strUrl = 'http://oauth.vk.com/authorize?client_id=4225742&scope=8&redirect_uri=http://oauth.vk.com/blank.html&display=wap&response_type=token';
 var strUrlApi = 'https://api.vk.com/method/';
 
 var appInit = require('./appInit');
-import $ = require('jquery');
-import _ = require('underscore');
 
 export function openLoginWindow(parentWindow, onClosedCallback) {
     var intervalId = parentWindow.setInterval(hashUpdate, 500);
--- test/appProxyTests.ts
+++ test/appProxyTests.ts
@@ -4,34 +4,38 @@ import strings = require('../app_modules/strings');
 
 describe("appProxy", () => {
 
-    class ProxyProviderDirect {
+    class ProxyProviderDirect implements appProxy.ProxyUrlProvider {
         getProxyForURL(url: string) {
             return "DIRECT";
         };
     }
-    
-    class ProxyProviderProxy {
+
+    class ProxyProviderProxy implements appProxy.ProxyUrlProvider {
         static host: string = "127.0.0.1";
         static port: string = "3128";
-        
+
         getProxyForURL(url: string) {
             return strings.format("PROXY {0}:{1}", ProxyProviderProxy.host, ProxyProviderProxy.port);
         };
     }
-        
+
     const url: string = "http://localhost";
 
     it("appProxy.makeHttpRequest - DIRECT", () => {
         appProxy.init(new ProxyProviderDirect());
+        
         var proxyDescriptor = appProxy.makeHttpRequest(url);
+        
         assert.equal(proxyDescriptor.host, "localhost", "proxyDescriptor.host");
         assert.equal(proxyDescriptor.path, "/", "proxyDescriptor.path");
         assert.equal(proxyDescriptor.port, null, "proxyDescriptor.port");
     });
-    
+
     it("appProxy.makeHttpRequest - PROXY", () => {
         appProxy.init(new ProxyProviderProxy());
+        
         var proxyDescriptor = appProxy.makeHttpRequest(url);
+        
         assert.equal(proxyDescriptor.host, ProxyProviderProxy.host, "proxyDescriptor.host");
         assert.equal(proxyDescriptor.path, url, "proxyDescriptor.path");
         assert.equal(proxyDescriptor.port, ProxyProviderProxy.port, "proxyDescriptor.port");
