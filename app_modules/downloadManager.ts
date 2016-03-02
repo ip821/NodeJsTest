7313b5b4b6da9e7159e53002288c033c02c98a7e
ip821 <ip821@yandex.ru>
2016-03-02 21:54:56 +0200

    Refactoring.

--- app_modules/downloadManager.ts
+++ app_modules/downloadManager.ts
@@ -15,30 +15,6 @@ export class DownloadManager {
         this.eventHandler = eventHandler;
     }
 
-    downloadInternal = (url, dest, streamSize) => {
-        var dataLength = 0;        
-        var file = fs.createWriteStream(dest);
-        var request = http.get(appProxy.makeHttpRequest(url), (response) => {
-            response.on('data', (data) => {
-                file.write(data);
-                dataLength += data.length;
-                this.eventHandler.onDownloadManagerProgress(dataLength, streamSize);
-            })
-                .on('end', () => {
-                    file.end();
-                    file.close();
-                    console.log('Downloading DONE: ' + url);
-                    this.eventHandler.onDownloadManagerCompleted(undefined);
-                })
-                .on('error', (err) => {
-                    console.log('Downloading FAIL: ' + url);
-                    file.close();
-                    fs.unlink(dest);
-                    this.eventHandler.onDownloadManagerCompleted(err);
-                });
-        });
-    }
-
     download = (url, dest) => {
         console.log('Downloading START: ' + url);
         var headerRequestOptions = appProxy.makeHttpRequest(url);
@@ -47,27 +23,40 @@ export class DownloadManager {
             var streamSize = 0;
             if (headersResponse.headers) {
                 streamSize = parseInt(headersResponse.headers['content-length']);
-                fs.exists(dest, (exists) => {
-                    if (exists) {
-                        fs.stat(dest, (err, stats) => {
-                            var fileSize = stats.size;
-                            console.log(stringUtils.format('Stream size: {0}, File size: {1}', streamSize, fileSize));
-                            headerRequest.end();
-                            if (streamSize === fileSize) {
-                                console.log('Skipping ' + dest);
-                                this.eventHandler.onDownloadManagerCompleted(undefined);
-                                return;
-                            }
-                            this.downloadInternal(url, dest, streamSize);
-                        });
+                var destExists = fs.existsSync(dest);
+                if (destExists) {
+                    var destStat = fs.statSync(dest);
+                    var fileSize = destStat.size;
+                    console.log(stringUtils.format('Stream size: {0}, File size: {1}', streamSize, fileSize));
+                    headerRequest.end();
+                    if (streamSize === fileSize) {
+                        console.log('Skipping ' + dest);
+                        this.eventHandler.onDownloadManagerCompleted(undefined);
                         return;
                     }
-                    this.downloadInternal(url, dest, streamSize);
-                });
-                return;
+                }
             }
-
-            this.downloadInternal(url, dest, streamSize);
+            var dataLength = 0;
+            var file = fs.createWriteStream(dest);
+            var request = http.get(appProxy.makeHttpRequest(url), (response) => {
+                response.on('data', (data) => {
+                    file.write(data);
+                    dataLength += data.length;
+                    this.eventHandler.onDownloadManagerProgress(dataLength, streamSize);
+                })
+                    .on('end', () => {
+                        file.end();
+                        file.close();
+                        console.log('Downloading DONE: ' + url);
+                        this.eventHandler.onDownloadManagerCompleted(undefined);
+                    })
+                    .on('error', (err) => {
+                        console.log('Downloading FAIL: ' + url);
+                        file.close();
+                        fs.unlink(dest);
+                        this.eventHandler.onDownloadManagerCompleted(err);
+                    });
+            });
         });
     }
 }
