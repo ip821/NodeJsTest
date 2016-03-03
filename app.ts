cb4927fa2bbf4c0c79571d8964903b1c7f0e8b01
igor <igor>
2016-03-03 16:10:10 +0200

    Moving to electron.

--- app.ts
+++ app.ts
@@ -1,16 +1,15 @@
-import gui = require('nw.gui');
 import {Controller} from "./app/controller";
 import proxy = require('./app_modules/proxy');
 var init = require('./app_modules/init');
 
-init.initJQuery(window, this);
-init.initXMLHttpRequest(this);
+// init.initJQuery(window, this);
+// init.initXMLHttpRequest(this);
 
 // gui.Window.get().show();
 // gui.Window.get().showDevTools();
 // gui.Window.get().focus();
 
-proxy.init(gui.App);
+//proxy.init(gui.App);
 
 $(document).ready(() => {
     var controller = new Controller();
