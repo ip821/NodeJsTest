d8aaeffa62c09369dd98187d43515e78979ad65f
ip821 <ip821@yandex.ru>
2016-03-03 21:27:49 +0200

    Refactoring.

--- boot.js
+++ boot.js
@@ -23,14 +23,6 @@ function createWindow() {
     }
 
     mainWindow.focus();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
 }
 
 // This method will be called when Electron has finished
