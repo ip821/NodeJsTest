9f9564e5450a9437be219db767ca9684fb7453fd
ip821 <ip821@yandex.ru>
2016-03-05 14:47:59 +0200

    ref

--- app.ts
+++ app.ts
@@ -1,13 +1,8 @@
 ///<reference path="node_modules/angular2/typings/browser.d.ts"/>
-import {Controller} from "./app/controller";
-import {ListDownloader, IListDownloaderEventHandler} from "./app/list_downloader";
-import {View, IViewEventHandler} from "./app/view";
-import {DownloadManager} from './app_modules/download_manager';
-import {VkApi, IAudioListItem} from './app_modules/vkapi';
 import 'zone.js';
 import 'reflect-metadata';
-import {Injector, Injectable, Component} from "angular2/core";
 import {bootstrap} from 'angular2/platform/browser';
+import {Controller} from "./app/controller";
 
 $(window.document).ready(() => {
     bootstrap(Controller)
