afae90f89d752a11384d4038579867f21b3993e2
igor <igor>
2016-03-07 14:59:50 +0200

    tests moved to jasmine

--- spec/mainview_spec.ts
+++ spec/mainview_spec.ts
@@ -1,57 +1,57 @@
-import assert = require('assert');
 import {ListDownloader} from "../services/list_downloader";
 import {MainViewComponent} from "../components/mainview_component";
+import {HeaderComponent} from "../components/header_component";
 import {VkApi, IAudioListItem} from '../services/vkapi';
 import {DownloadManager} from '../services/download_manager';
 import {Mock, It, MockBehavior} from "typemoq";
 import 'zone.js';
 import 'reflect-metadata';
+var jasmine = require('jasmine');
 import {Injector, Injectable, NgZone, provide} from "angular2/core";
-//import {MockNgZone} from "angular2/testing";
+import {describe,expect,it,xit, inject, injectAsync, beforeEachProviders, MockNgZone, TestComponentBuilder, ComponentFixture} from 'angular2/testing';
 
-describe("controller", () => {
+describe("mainview", () => {
 
     var container: Injector;
     var listDownloaderMock: Mock<ListDownloader>;
     var vkApiMock: Mock<VkApi>;
     var downloadManagerMock: Mock<DownloadManager>;
-    var ngZoneMock: Mock<NgZone>;
 
-    beforeEach(() => {
+    beforeEachProviders(() =>
+    {
         downloadManagerMock = Mock.ofType(DownloadManager);
         listDownloaderMock = Mock.ofType(ListDownloader, MockBehavior.Loose, downloadManagerMock.object);
         vkApiMock = Mock.ofType(VkApi);
-        ngZoneMock = Mock.ofType(NgZone, MockBehavior.Loose, {enableLongStackTrace: true});
-        ngZoneMock.setup(c => c.run(It.isAny())).callback((fn) => fn());
-
-        container = Injector.resolveAndCreate([
-            provide(NgZone, { useValue: ngZoneMock.object }),
-            provide(ListDownloader, { useValue: listDownloaderMock.object }),
-            provide(VkApi, { useValue: vkApiMock.object }),
-            provide(DownloadManager, { useValue: downloadManagerMock.object }),
-            MainViewComponent
-        ]);
+        
+        return [
+            MainViewComponent,
+            provide(DownloadManager, {useValue: downloadManagerMock.object}),
+            provide(ListDownloader, {useValue: listDownloaderMock.object}),
+            provide(VkApi, {useValue: vkApiMock.object}),
+            provide(NgZone, {useClass: MockNgZone}),
+        ]
     });
-
-    it("should start download on sync command", () => {
+    
+    it("should start download on sync command", inject([MainViewComponent], (mainView) => {
         var isCalled = false;
         listDownloaderMock.setup(c => c.startDownload(It.isAny())).callback(() => isCalled = true);
 
-        var mainView = container.get(MainViewComponent);
+        var headerMock = Mock.ofType(HeaderComponent);
+        headerMock.setup(c => c.setRunningState(It.isAny()));
+
         mainView.audioList = [];
+        mainView._header = headerMock.object;
         mainView.onViewSyncClick();
+        expect(isCalled).toBe(true, "startDownload should be called");
+    }));
 
-        assert.equal(isCalled, true, "startDownload should be called");
-    });
-
-    it("should stop download on stop command", () => {
+    it("should stop download on stop command", inject([MainViewComponent], (mainView) => {
         var isCalled = false;
         listDownloaderMock.setup(c => c.stopDownload()).callback(() => isCalled = true);
 
-        var mainView = container.get(MainViewComponent);
         mainView.audioList = [];
         mainView.onViewStopClick();
 
-        assert.equal(isCalled, true, "stopDownload should be called");
-    });
+        expect(isCalled).toBe(true,  "stopDownload should be called");
+    }));
 });
